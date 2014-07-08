d4c393eddaf5380934b601f0ba1beee30f8796bb
Tansien <tansien@zombies.nu>
2014-07-09 01:30:05 +0200

    Version bump.

--- source/ZombiesNU.DayZeroLauncher.App/Properties/AssemblyInfo.cs
+++ source/ZombiesNU.DayZeroLauncher.App/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Resources;
 
 [assembly: AssemblyCompany("ZOMBIES.NU")]
 [assembly: AssemblyProduct("DayZero Launcher")]
-[assembly: AssemblyInformationalVersion("1.4.2")]
+[assembly: AssemblyInformationalVersion("1.4.5")]
 [assembly: AssemblyCopyright("Copyright 2013-2014 ZOMBIES.NU")]
 [assembly: AssemblyTitle("DayZero Launcher")]
-[assembly: AssemblyVersion("1.4.2.53")]
-[assembly: AssemblyFileVersion("1.4.2.53")]
+[assembly: AssemblyVersion("1.4.5.62")]
+[assembly: AssemblyFileVersion("1.4.5.62")]
 [assembly: NeutralResourcesLanguage("")]
