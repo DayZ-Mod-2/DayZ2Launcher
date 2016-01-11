083a6f9c19dade900d169e9edff92742558171b5
Tansien <tansien@zombies.nu>
2016-01-11 20:01:24 +0100

    Updating copyright information.

--- source/ZombiesNU.DayZeroLauncher.App/Properties/AssemblyInfo.cs
+++ source/ZombiesNU.DayZeroLauncher.App/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 [assembly: AssemblyCompany("ZOMBIES.NU")]
 [assembly: AssemblyProduct("DayZero Launcher")]
 [assembly: AssemblyInformationalVersion("1.4.5")]
-[assembly: AssemblyCopyright("Copyright 2013-2014 ZOMBIES.NU")]
+[assembly: AssemblyCopyright("Copyright 2013-2016 ZOMBIES.NU")]
 [assembly: AssemblyTitle("DayZero Launcher")]
 [assembly: AssemblyVersion("1.4.5.62")]
 [assembly: AssemblyFileVersion("1.4.5.62")]
