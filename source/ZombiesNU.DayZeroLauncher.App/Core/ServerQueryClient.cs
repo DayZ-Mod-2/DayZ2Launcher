22c18ee886e1f6007b32f947e261e03d029cde60
Tansien <tansien@zombies.nu>
2016-01-15 21:12:36 +0100

    Not used, and thus not needed.

--- source/ZombiesNU.DayZeroLauncher.App/Core/ServerQueryClient.cs
+++ source/ZombiesNU.DayZeroLauncher.App/Core/ServerQueryClient.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
-using System.Net.Sockets;
 using System.Text;
 
 namespace zombiesnu.DayZeroLauncher.App.Core
